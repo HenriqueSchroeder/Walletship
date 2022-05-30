fce1e7b5ba528ff808b3d0773723a17538a4e5d1
Henrique Schroeder <henriqueschroederhs@gmail.com>
2022-05-30 11:40:41 -0300

    feat(backend - modules): added crate wallet

---
+++ packages/backend/src/modules/wallet/service/create.ts
@@ -0,0 +1,59 @@
+import db from '~/db/prisma'
+import { logger } from '~/common/logger'
+
+import { createWalletParams } from './types'
+
+/**
+ * Creates the wallets.
+ */
+export const createWallet = async (params: createWalletParams) => {
+  /**
+   * Log.
+   */
+  logger.info('==== CRIA CARTEIRA =====')
+
+  /**
+   * Extracts values.
+   */
+  const { userId, title, content } = params
+
+  /**
+   * Consulting the User ID.
+   */
+  const userExists = await db.user.findUnique({
+    where: {
+      id: userId
+    }
+  })
+
+  /**
+   * If the user does not exist.
+   */
+  if (!userExists) {
+    /**
+     * Logs.
+     */
+    logger.info('Usuario não existe')
+    logger.info('==== CRIA CARTEIRA =====')
+    return { status: 'ERROR', description: 'User does not exist', id: '' }
+  }
+
+  /**
+   * Creating the wallet.
+   */
+  const wallet = await db.wallet.create({
+    data: {
+      title,
+      content,
+      userId
+    }
+  })
+
+  /**
+   * Logs.
+   */
+  logger.info('Carteira criada')
+  logger.info('==== CRIA CARTEIRA =====')
+
+  return { status: 'OK', description: 'Created wallet', id: wallet.id }
+}
---
+++ packages/backend/src/modules/wallet/service/types.ts
@@ -0,0 +1,5 @@
+export type createWalletParams = {
+  userId: string
+  title: string
+  content: string
+}
