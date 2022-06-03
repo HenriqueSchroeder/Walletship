8639f2aa67e26b1c09baa65810c5f4fa6b959428
Henrique Schroeder <henriqueschroederhs@gmail.com>
2022-06-03 11:58:54 -0300

    fix: updated workflow

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -61,7 +61,6 @@ export const server = async () => {
    * Increase response size.
    */
   app.use(express.json({ limit: '50mb' }))
-  // app.use(express.urlencoded())
 
   /**
    * Enable security connection.
