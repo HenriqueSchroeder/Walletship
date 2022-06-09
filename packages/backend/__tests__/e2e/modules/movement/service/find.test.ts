948fd902030b379e15e53db0e3170329c479e6f1
Henrique Schroeder <henriqueschroederhs@gmail.com>
2022-06-09 17:56:38 -0300

    fix(test - movement): updated import

--- packages/backend/__tests__/e2e/modules/movement/service/find.test.ts
+++ packages/backend/__tests__/e2e/modules/movement/service/find.test.ts
@@ -1,11 +1,11 @@
 import { loginUser } from 'test/helpers/login-setup'
 import { runInTenant } from 'test/helpers/tenant'
+import { WalletBuilder } from 'test/helpers/builders/wallet'
+import { MovementBuilder } from 'test/helpers/builders/movement'
+import { CategoryBuilder } from 'test/helpers/builders/category'
 import { client, closeServer, startServer } from 'test/helpers/server'
 
 import { findMovementsUserQuery } from './queries'
-import { MovementBuilder } from 'test/helpers/builders/movement'
-import { WalletBuilder } from 'test/helpers/builders/wallet'
-import { CategoryBuilder } from 'test/helpers/builders/category'
 
 describe('[E2E] Mostra todas as movimentações do usuario', () => {
   beforeAll(startServer)
